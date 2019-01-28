0899333596ba04666435c79e173d068c66bc9269
Phuc Tran <phucdn2005@gmail.com>
2019-01-28 15:33:19 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+import numpy as np
+import pandas as pd
+
+def main():
+    # import them files
+    locations = pd.read_csv('location.csv')
+    pickup_times = pd.read_csv('pickup_times.csv')
+
+    # filter them out by date, hours, group them by location
+
+
+    # calculate the median
+
+if __name__ == 'main':
+    main()
